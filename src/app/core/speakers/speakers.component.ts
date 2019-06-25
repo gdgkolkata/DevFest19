ef2ecbbd9de90ce57a6117239a3e27e459339dc7
ACCLARISCORP\Debmallya.Bhattachar <debmallya.bhattacharya@acclaris.com>
2019-06-25 20:55:08 +0530

    forming filters

--- src/app/core/speakers/speakers.component.ts
+++ src/app/core/speakers/speakers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AppService } from 'src/app/shared/services/app.service';
 import { Observable } from 'rxjs';
+import { AppService } from 'src/app/shared/services/app.service';
 import { Speaker } from 'src/app/shared/interfaces/speaker';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-speakers',
@@ -10,10 +11,23 @@ import { Speaker } from 'src/app/shared/interfaces/speaker';
 })
 export class SpeakersComponent implements OnInit {
   speakers$: Observable<Speaker[]>;
+  isLoading: boolean;
+  filters: string[] = ['Cloud', 'Android', 'Web'];
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
-    this.speakers$ = this.appService.getSpeakers();
+    this.isLoading = true;
+    this.speakers$ = this.appService
+      .getSpeakers()
+      .pipe(finalize(() => (this.isLoading = false)));
+  }
+
+  removeFilter(index: number) {
+    this.filters.splice(index, 1);
+  }
+
+  clearAll() {
+    this.filters = [];
   }
 }
