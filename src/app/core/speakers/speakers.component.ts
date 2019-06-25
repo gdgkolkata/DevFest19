bf5d3592ec2acb6702a9eca0d4075b090570da6b
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-06-26 05:22:50 +0530

    filter improved

--- src/app/core/speakers/speakers.component.ts
+++ src/app/core/speakers/speakers.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { AppService } from 'src/app/shared/services/app.service';
 import { Speaker } from 'src/app/shared/interfaces/speaker';
-import { finalize } from 'rxjs/operators';
+
+interface Filter {
+  name: string;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-speakers',
@@ -12,7 +17,13 @@ import { finalize } from 'rxjs/operators';
 export class SpeakersComponent implements OnInit {
   speakers$: Observable<Speaker[]>;
   isLoading: boolean;
-  filters: string[] = ['Cloud', 'Android', 'Web'];
+  isFilterPopup: boolean;
+  filters: Filter[] = [
+    { name: 'Cloud', selected: false },
+    { name: 'Android', selected: false },
+    { name: 'Web', selected: false }
+  ];
+  filters$ = new BehaviorSubject<Filter[]>([]);
 
   constructor(private appService: AppService) {}
 
@@ -23,11 +34,23 @@ export class SpeakersComponent implements OnInit {
       .pipe(finalize(() => (this.isLoading = false)));
   }
 
-  removeFilter(index: number) {
-    this.filters.splice(index, 1);
+  toggleFilterPopup() {
+    this.isFilterPopup = !this.isFilterPopup;
+  }
+
+  private triggerFilterChanges() {
+    this.filters$.next(this.filters.filter(x => x.selected));
   }
 
   clearAll() {
-    this.filters = [];
+    this.isFilterPopup = false;
+    this.filters.forEach(filter => (filter.selected = false));
+    this.triggerFilterChanges();
+  }
+
+  onFilterToggle(filterName: string) {
+    const index = this.filters.findIndex(x => x.name === filterName);
+    this.filters[index].selected = !this.filters[index].selected;
+    this.triggerFilterChanges();
   }
 }
