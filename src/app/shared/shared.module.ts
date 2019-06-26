097411d22dc4d3d2d3587427841a25788e8b7614
ACCLARISCORP\Debmallya.Bhattachar <debmallya.bhattacharya@acclaris.com>
2019-06-26 14:52:44 +0530

    added team page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,11 +8,24 @@ import { SpeakerComponent } from './components/speaker/speaker.component';
 import { FilterComponent } from './components/filter/filter.component';
 import { PopupComponent } from './components/popup/popup.component';
 import { RoundImgComponent } from './components/round-img/round-img.component';
+import { TeamMemberComponent } from './components/team-member/team-member.component';
 
 @NgModule({
-  declarations: [DeferLoadDirective, SpeakerComponent, FilterComponent, PopupComponent, RoundImgComponent],
+  declarations: [
+    DeferLoadDirective,
+    SpeakerComponent,
+    FilterComponent,
+    PopupComponent,
+    RoundImgComponent,
+    TeamMemberComponent
+  ],
   entryComponents: [PopupComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [MaterialModule, SpeakerComponent, FilterComponent]
+  exports: [
+    MaterialModule,
+    SpeakerComponent,
+    FilterComponent,
+    TeamMemberComponent
+  ]
 })
 export class SharedModule {}
