93477613686930ee1e4bd3d91a198d3fcc245563
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-06-28 19:25:16 +0530

    added dates

--- src/app/core/schedule/schedule.component.ts
+++ src/app/core/schedule/schedule.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { AppService } from 'src/app/shared/services/app.service';
 import { Schedule } from 'src/app/shared/interfaces/schedule';
@@ -10,15 +9,16 @@ import { Schedule } from 'src/app/shared/interfaces/schedule';
   styleUrls: ['./schedule.component.scss']
 })
 export class ScheduleComponent implements OnInit {
-  schedule$: Observable<Schedule[]>;
+  schedules: Schedule[] = [];
   isLoading: boolean;
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
     this.isLoading = true;
-    this.schedule$ = this.appService
+    this.appService
       .getSchedule()
-      .pipe(finalize(() => (this.isLoading = false)));
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe(schedules => (this.schedules = schedules));
   }
 }
