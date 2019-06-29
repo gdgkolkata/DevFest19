ab6d0769463edd949b27eb030511a1f9131a5832
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-06-29 12:49:46 +0530

    topBar improved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Renderer2,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/scrolling';
 import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
 import { Observable } from 'rxjs';
 import { map, distinctUntilChanged, filter } from 'rxjs/operators';
 
@@ -19,7 +27,12 @@ export class AppComponent implements OnInit, AfterViewInit {
   siteName: string;
   navLinks: NavLink[];
   isBreakpoint$: Observable<boolean>;
-  isHome: boolean;
+  private isHome: boolean;
+  @ViewChild('topBar', { read: ElementRef, static: true }) topBarEl: ElementRef;
+  topBar: MatToolbar;
+  @ViewChild('homeLink', { read: ElementRef, static: false }) homeLinkEl: ElementRef;
+  homeLink: HTMLAnchorElement;
+  isHomeLinkHidden: boolean;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -41,42 +54,70 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.router.events.pipe(
-      filter((x: RouterEvent) => x instanceof NavigationEnd),
-      map((y: NavigationEnd) => y.urlAfterRedirects === '/home')
-    ).subscribe(val => this.isHome = val);
+    this.topBar = this.topBarEl.nativeElement;
+    this.router.events
+      .pipe(
+        filter((x: RouterEvent) => x instanceof NavigationEnd),
+        map((y: NavigationEnd) => y.urlAfterRedirects === '/home')
+      )
+      .subscribe(val => {
+        this.isHome = val;
+        this.isHomeLinkHidden = val;
+        const colorCode = this.isHome ? 255 : 0;
+        this.renderer.setAttribute(
+          this.topBar,
+          'style',
+          `color: rgba(${colorCode},${colorCode},${colorCode},0.87)`
+        );
+      });
     this.isBreakpoint$ = this.breakpointObserver
       .observe([Breakpoints.Small, Breakpoints.XSmall])
       .pipe(map(result => result.matches));
   }
 
   ngAfterViewInit() {
+    this.homeLink = this.homeLinkEl.nativeElement;
     const completionPixel = 60;
     this.scrollDispatcher
       .scrolled()
       .pipe(
         map((el: CdkScrollable) => {
           const offsetTop = el.measureScrollOffset('top');
-          function calculateProperty(propFinalValue: number): number {
-            const value = (offsetTop * propFinalValue) / completionPixel;
-            return value > propFinalValue ? propFinalValue : value;
+          const calcProp = (propFinalValue: number) =>
+            offsetTop >= completionPixel ? propFinalValue : 0;
+          // function calculateProperty(propFinalValue: number): number {
+          //   const value = (offsetTop * propFinalValue) / completionPixel;
+          //   return value > propFinalValue ? propFinalValue : value;
+          // }
+          const background = `background: rgba(255,255,255,${calcProp(1)});`;
+          let color = '';
+          let colorCode = 255;
+          if (this.isHome) {
+            colorCode = 255 - calcProp(255);
+            color = `color: rgba(${colorCode},${colorCode},${colorCode},0.87);`;
           }
-          const background =
-            `background: rgba(255,255,255,${calculateProperty(1)})`;
           const boxShadow =
             'box-shadow: ' +
-            `0 2px 04px -1px rgba(0,0,0,${calculateProperty(0.2)}), ` +
-            `0 4px 05px  0   rgba(0,0,0,${calculateProperty(0.14)}), ` +
-            `0 1px 10px  0   rgba(0,0,0,${calculateProperty(0.12)})`;
+            `0 2px 04px -1px rgba(0,0,0,${calcProp(0.2)}), ` +
+            `0 4px 05px  0   rgba(0,0,0,${calcProp(0.14)}), ` +
+            `0 1px 10px  0   rgba(0,0,0,${calcProp(0.12)})`;
           return {
-            style: `${background};${boxShadow}`,
-            topBar: el.getElementRef().nativeElement.firstElementChild
+            isHomeLinkHidden: colorCode === 255,
+            style: `${background}${color}${boxShadow}`
           };
         }),
         distinctUntilChanged((x, y) => x.style === y.style)
       )
-      .subscribe(({ style, topBar }) =>
-        this.renderer.setAttribute(topBar, 'style', style)
-      );
+      .subscribe(({isHomeLinkHidden, style}) => {
+        if (this.isHome) {
+          this.isHomeLinkHidden = isHomeLinkHidden;
+          if (this.isHomeLinkHidden) {
+            this.renderer.addClass(this.homeLink, 'no-logo');
+          } else {
+            this.renderer.removeClass(this.homeLink, 'no-logo');
+          }
+        }
+        this.renderer.setAttribute(this.topBar, 'style', style);
+      });
   }
 }
